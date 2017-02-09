88ef3ab96d4d0b0033211a475acdd143cf5a8ac3
sugandha11 <sugandha11@gmail.com>
2017-02-09 12:31:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article1.html',function (req, res){
+app.get('/article1',function (req, res){
     res.send('Article one is requested and will be served here');
 });
 
