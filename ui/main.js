705cdcae55e802448e822cae3776c37c0062e957
sugandha11 <sugandha11@gmail.com>
2017-02-15 05:20:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,9 @@
 console.log('Loaded!');
 
-var element = document.getElementById('main-text');
+/*
 // change the text of document
-element.innerHTML = 'New Value';
+var element = document.getElementById('main-text');
+element.innerHTML = 'New Value';*/
 
 // move the image 
 var img = document.getElementById('madi');
