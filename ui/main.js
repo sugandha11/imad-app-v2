c04070ac17561e325158fa6310f2cdfeaed7f011
sugandha11 <sugandha11@gmail.com>
2017-02-15 04:32:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,8 @@ var marginLeft = 0;
 function moveRight (){
     marginLeft = marginLeft + 2;
     img.style.marginLeft = marginLeft + 'px';
+    if (marginLeft > 2000 ) 
+        marginLeft = 0 ;
 }
  img.onclick = function (){
      var interval = setInterval ( moveRight, 50);
