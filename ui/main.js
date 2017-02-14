110aa73f279e83e2c8eafe1e57b8ca82fe9a2be4
sugandha11 <sugandha11@gmail.com>
2017-02-15 05:06:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 // move the image 
-/*var img = document.getElementById('madi');
+var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight (){
     marginLeft = marginLeft + 2;
@@ -19,17 +19,5 @@ function moveRight (){
  img.onclick = function (){
      var interval = setInterval ( moveRight, 50);
      //img.style.marginLeft = '100px';
- }; */
+ }; 
  
- var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-marginLeft=marginLeft + 10;
-img.style.marginLeft=marginLeft + 'px';
-
-}
-
-img.onclick=function(){
-var interval=setInterval(moveRight , 100);
-};
-
