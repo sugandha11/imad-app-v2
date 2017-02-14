01bc33cd6d2542490245814d15aacd5ade1fb228
sugandha11 <sugandha11@gmail.com>
2017-02-15 04:01:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 // change the text of document
 element.innerHTML = 'New Value';
+
+// move the image 
+var img = document.getElementById('madi');
+ img.onClick = function (){
+     img.style.marginLeft = '100px';
+ };
