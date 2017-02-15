196d92cb846e3ffb2b867c9fce71cd24cb1d275b
sugandha11 <sugandha11@gmail.com>
2017-02-15 05:37:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 var counter=0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
-    res.send(counter.toSting());
+    res.send(counter.toString());
 });
 
 app.get('/', function (req, res) {
