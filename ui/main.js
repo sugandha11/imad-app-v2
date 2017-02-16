21e39fbca9b371936eac7279a4413444e2bf765f
sugandha11 <sugandha11@gmail.com>
2017-02-16 06:17:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,6 +23,6 @@ button.onclick = function(){
        
     };
     //make the request
-    request.open('GET','http://sugandha11.imad.hasura-app.io/counter',true)
+    request.open('GET','http://sugandha11.imad.hasura-app.io/counter',true);
     request.send(null);
 };
