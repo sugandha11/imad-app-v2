34bcd9aa334f4b0ea74f6fc850c48dc6cc749419
sugandha11 <sugandha11@gmail.com>
2017-02-16 06:05:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,25 @@ var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick = function(){
-    counter +=1;
-    var span=document.getElementById('count');
-    span.innerHTML= counter.toString();
+    
+    // Create a request object 
+    var reauuest = new XMLHttpRequest ();
+    
+   //capture the responce and store it in a variable
+    request.onreadystatechange = function (){
+      
+       if (request.status === XMLHttpRequest.DONE){
+           
+             //take some action
+             if (request.status === 200){
+                 var counter =request.responceText;
+                 var span=document.getElementById('count');
+                 span.innerHTML= counter.toString();
+         }
+       }
+       
+    };
+    //make the request
+    request.open('GET','http://sugandha11.imad.hasura-app.io/counter',true)
+    request.send(null);
 };
