fd2d00e91d417f4f762e32a9a51d262b74f48e89
sugandha11 <sugandha11@gmail.com>
2017-02-21 00:35:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,12 +27,12 @@ button.onclick = function(){
 };
 
 //submit Name
-var nameInput = document.getElementById('name');
-//var name = nameInput.value;
+var nameInput = document.getElementById('name1');
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function (){
-     var names= ['name1','name2','name3', 'name4'];
+    var names= ['name1','name2','name3', 'name4'];
     var list='';
     for (var i=0; i < names.length; i++) {
         list += '<li>' + names[i] + '</li>';
