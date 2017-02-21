1f9b8208bbb0257dade524f5da55275daf394046
sugandha11 <sugandha11@gmail.com>
2017-02-21 23:46:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -56,7 +56,7 @@ submit.onclick = function (){
        }
     };
     
-    request.open('GET','http://sugandha11.imad.hasura-app.io/submit-name?name='+name,true);
+    request.open('GET','http://sugandha11.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
 };
     // make request to the server and send name
