d5f29717d72e5fb4a4f52fee1b409b19ee7fbebb
sugandha11 <sugandha11@gmail.com>
2017-03-05 13:23:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,11 +27,11 @@ app.get('/test-db', function (req, res) {
          res.status(500).send(err.toString());
      }
      else {
-         res.send(JSON.stringify(result));
+         res.send(JSON.stringify(result.rows));
      }
      });
     
-};
+});
 
 
 var counter=0;
